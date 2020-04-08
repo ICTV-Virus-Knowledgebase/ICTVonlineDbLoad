752c13d9019b23de07c996928f93ff05e8f6a487
Curtis Hendrickson <curtish@uab.edu>
2020-04-08 16:46:22 -0500

    update hardcoded MSL to 34

--- 9yb. patch make unique species the type species.sql
+++ 9yb. patch make unique species the type species.sql
@@ -3,7 +3,7 @@
 --
 select report='unique species in genera w/o type species'
 	, genus as genus, count(species) as species_count, sum(is_type_species) as tspecies_ct, max(species) as species
-from MSL_export_fast where msl_release_num = 33 AND genus <> ''
+from MSL_export_fast where msl_release_num = 34 AND genus <> ''
 group by genus
 having sum(is_type_species) < 1  and count(species) = 1
 order by species_count, genus
@@ -37,7 +37,7 @@ where name in (
 	group by genus
 	having sum(is_type_species) < 1  and count(species) = 1
 	--order by species_count, genus
-) and tree_id = dbo.get_tree_id_of_latest_msl()
+) and tree_id = 34
 
 update taxonomy_node set 
 	is_ref=1
