72043174929421a4242a96da76ee5e445bb77832
Curtis Hendrickson <curtish@uab.edu>
2020-04-21 14:07:30 -0500

    Add rebuild_node_merge_split, and post-query

--- 9ya1.patch - remove Unyawo genus and species.sql
+++ 9ya1.patch - remove Unyawo genus and species.sql
@@ -21,6 +21,15 @@ from taxonomy_node
 where msl_release_num=dbo.udf_getMSL(NULL) 
 and name in ('Unyawo', 'Xylella virus Paz')
 
+-- view context
+select report='before', n.taxnode_id, n.level_id, n.lineage, n._numKids, targ=(case when n.name in ('Unyawo', 'Xylella virus Paz') then '<<<===' else '' end)
+from taxonomy_node n
+join taxonomy_node t on n.left_idx between t.left_idx and t.right_idx and t.tree_id = n.tree_id
+where t.name in ('Autographiviridae')
+order by n.left_idx
+
+--exec rebuild_delta_nodes
+exec rebuild_node_merge_split
 
 
 --commit transaction 
