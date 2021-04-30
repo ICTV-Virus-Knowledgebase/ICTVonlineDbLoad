cafc8aba6fe5cab60f528859cd7d5ae05ebb8e70
Curtis Hendrickson <curtish@uab.edu>
2021-04-30 11:00:24 -0500

    get rid of isDone constraint

--- 0.b1. rename old load_next_msl.sql
+++ 0.b1. rename old load_next_msl.sql
@@ -14,13 +14,21 @@ GO
 --
 -- rename default constraints - that's a global namespace (ICK!)
 --
-exec sp_rename 'DF_load_next_msl__msl_release_num', 'DF_load_next_msl__msl_release_num_34b';  
-exec sp_rename 'DF_load_next_msl_isDone', 'DF_load_next_msl_isDone_34b';  
+exec sp_rename 'DF_load_next_msl__msl_release_num', 'DF_load_next_msl__msl_release_num_35';  
 GO
 
 -- 
 -- rename table
-EXEC sp_rename 'load_next_msl', 'load_next_msl_34b';  
+EXEC sp_rename 'load_next_msl', 'load_next_msl_35';  
 --EXEC sp_rename 'load_next_msl_34b', 'load_next_msl';  
 
 go
+
+--
+-- Verify
+--
+SELECT name, SCHEMA_NAME(schema_id) AS schema_name, type_desc, type  
+FROM sys.objects  
+WHERE parent_object_id = (OBJECT_ID('load_next_msl_35'))   
+AND type IN ('D','C','F', 'PK');   
+GO 
