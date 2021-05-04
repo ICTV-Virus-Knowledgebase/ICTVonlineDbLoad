6e4a0e1e557209526d0abca26f9486fa101ccfc3
Curtis Hendrickson <curtish@uab.edu>
2021-05-04 11:16:34 -0500

    get promote/demote working correctly; fix 2 taxa with action=new, but src* is NOT null
        fix code for prev_msl QC: 34 → (@msl-1)
        QC by action: OK
        QC by action,rank: error: 1 promote counted in both subfamily and genus (it's a promote, duh!)
        Go back and fix "move/rename/promote" code in "4.a.apply_create_actions_RANK_high_to_low.sql" to correctly set desintation rank. 
        Add "demote" as a valid action "0.b5.taxonomy_change_out_INSERT_demote.sql"
        change sort=59299 ; genus=>subgenus to _action='demote'
        change sort=113438 -- (change=new family); set all srcRANK fields to NULL. 
        change sort= 92052 -- (change=new family); set all srcRANK fields to NULL. 
        that way, this query returns only promotes and demotes
    
        select * from load_next_msl where isnull(_src_taxon_rank,_dest_taxon_rank) <>_dest_taxon_rank
    
        4.a.apply_create_actions_RANK_high_to_low.sql
        lots of code fixes to correctly report on actions that change rank levels.

--- 4c.qc_actions.sql
+++ 4c.qc_actions.sql
@@ -4,11 +4,30 @@
  *
  */
 DECLARE @msl int; SET @msl=(select distinct dest_msl_release_num from load_next_msl)
+print 'MSL: '+rtrim(@MSL)
 
+--
+-- MSL36 cleanups
+--
 
+--Add "demote" as a valid action "0.b5.taxonomy_change_out_INSERT_demote.sql"
+  --change sort=59299 ; genus=>subgenus to _action='demote'
+-- Got back and fix "move/rename/promote" code in "4.a.apply_create_actions_RANK_high_to_low.sql" to correctly set desintation rank. 
+  -- re-run move/rename/promote/demote for genera/genus
+-- remove "src" lineage info from sort=113438 (change=new family)
+/*
+update load_next_msl set 
+	srcRealm=NULL, srcSubRealm=NULL, srcKingdom=NULL,srcSubKingdom=NULL, srcPhylum=NULL, srcSubPhylum=NULL,srcClass=NULL,srcSubClass=NULL
+	,srcOrder=NULL,srcSubOrder=NULL, srcFamily=NULL, srcSubFamily=NULL, srcGenus=NULL, srcSubGenus=NULL, srcSpecies=NULL
+from load_next_msl
+where sort=113438 -- (change=new family)
+or    sort=92052  -- (change=new family)
+select 'AFTER src=NULL', * from load_next_msl where sort in (113438,92052)
+*/
 
 -- 
 -- summary counts by [ACTION]
+--
 select 
 	report='counts by [ACTION]'
 	,action=act.change, prevMSL=prev.ct, load_next_msl=new.ct, nextMSL=dest.ct
@@ -24,7 +43,7 @@ left outer join (
 left outer join (
 	-- prev-MSL: out_change
 	select  change=out_change, ct=count(*),title='prevMSL.out_action', col='out_change', msl_release_num
-	from taxonomy_node_names where msl_release_num=34/*(@msl-1)*/ and out_change is not null
+	from taxonomy_node_names where msl_release_num=(@msl-1) and out_change is not null
 	group by msl_release_num, out_change
 ) as prev on prev.change = act.change
 left outer join (
@@ -38,33 +57,40 @@ order by action
 --
 -- summary counts by [ACTION, RANK]
 --
+-- DECLARE @msl int; SET @msl=(select distinct dest_msl_release_num from load_next_msl); print 'MSL: '+rtrim(@MSL)
 select 
 	report='counts by [ACTION, RANK]'
-	, action=act.change, rank.name, prevMSL=prev.ct, load_next_msl=new.ct, nextMSL=dest.ct
+	, action=act.change, rank.name, dest_rank.name, prevMSL=prev.ct, load_next_msl=new.ct, nextMSL=dest.ct
 	, (case when  isnull(prev.ct,0)+isnull(dest.ct,0) = isnull(new.ct,0) then 'OK' else 'ERROR' end )
 from (select change from taxonomy_change_in union select change from taxonomy_change_out) as act
 join taxonomy_level as rank on 1=1
+join taxonomy_level as dest_rank on 1=1
 left outer join (
 	-- load_next_msl
-	select change=_action, rank, ct=count(*), title='load_next_msl', col='_action',  msl=dest_msl_release_num
+	select change=_action, rank=isnull(_src_taxon_rank,_dest_taxon_rank), dest_rank=_dest_taxon_rank,  ct=count(*), title='load_next_msl', col='_action',  msl=dest_msl_release_num
 	from load_next_msl
 	where isWrong is null
-	group by dest_msl_release_num, _action, rank
-) as new on new.change = act.change and new.rank = rank.name
+	group by dest_msl_release_num, _action, rank, _src_taxon_rank, _dest_taxon_rank
+) as new on new.change = act.change and new.rank = rank.name and new.dest_rank = dest_rank.name
 left outer join (
 	-- prev-MSL: out_change
-	select  change=out_change, rank, ct=count(*),title='prevMSL.out_action', col='out_change', msl_release_num
-	from taxonomy_node_names where msl_release_num=34/*(@msl-1)*/ and out_change is not null
-	group by msl_release_num, out_change, rank
-) as prev on prev.change = act.change and prev.rank = rank.name
+	-- DECLARE @msl int; SET @msl=(select distinct dest_msl_release_num from load_next_msl); print 'MSL: '+rtrim(@MSL)
+	--select change=prev.out_change, prev.rank, dest_rank=dest.rank,'>>>', prev.taxnode_id, prev.node_depth, prev.rank, prev.lineage, prev.out_target, dest.taxnode_id, dest.rank, dest.name, dest.node_depth
+	select  change=prev.out_change, prev.rank, dest_rank=dest.rank, ct=count(*),title='prevMSL.out_action', col='out_change', prev.msl_release_num
+	from taxonomy_node_names prev
+	left outer join taxonomy_node_names dest on prev.out_target in (dest.lineage, dest.name) and dest.msl_release_num = prev.msl_release_num + 1
+	where prev.msl_release_num=(@msl-1) and prev.out_change is not null
+	group by prev.msl_release_num, prev.out_change, prev.rank, dest.rank
+) as prev on prev.change = act.change and prev.rank = rank.name and prev.dest_rank = dest_rank.name
 left outer join (
 	-- MSL: in_change DECLARE @msl int; SET @msl=(select distinct dest_msl_release_num from load_next_msl)
-	select change=in_change, rank, ct=count(*), title='currMSL.in_action ', col='in_change=',  msl_release_num
+	-- DECLARE @msl int; SET @msl=(select distinct dest_msl_release_num from load_next_msl); print 'MSL: '+rtrim(@MSL)
+	select change=in_change, rank=rank, dest_rank=rank, ct=count(*), title='currMSL.in_action ', col='in_change=',  msl_release_num
 	from taxonomy_node_names where msl_release_num=@msl and in_change is not null
 	group by msl_release_num, in_change, rank
-) as dest on dest.change = act.change and dest.rank = rank.name
+) as dest on dest.change = act.change and dest.rank = rank.name and dest.dest_rank = dest_rank.name
 where isnull(isnull(prev.ct, new.ct),dest.ct) is not null
-order by action, rank.id
+order by action, rank.id, dest_rank.id
 
 --
 -- move qc with name check
@@ -115,25 +141,4 @@ from taxonomy_node where
 msl_release_num=34 and out_change='move'
 order by level_id 
 
-
---
--- data fixes
---
-
---
--- ICTV35: double entry: Banyangvirus >> rename>> Bandavirus 
--- This genus gets renamed twice, to the same thing (could be worse – at least the result is consistent).
--- I’ve marked the 2nd rename “isWrong”, so the associated proposal will be “2019.015M.zip” aka “2019.015M.Bandavirus.xlsx”
--- sort=[506,759]
---
-update load_next_msl 
-set isWrong='Same change already made in sort=506, proposal=2019.015M.Bandavirus.xlsx' 
-where sort = 759 and _dest_taxon_name = 'Bandavirus' 
-and isWrong is null
-
-
-update load_next_msl 
-set isWrong='Same change already made in sort=165, proposal=2019.005G.Monodnaviria.xlsx' 
-where sort = 1016 and _dest_parent_name='Tubulavirales' and _dest_taxon_name = 'Inoviridae' 
-and isWrong is null
 
