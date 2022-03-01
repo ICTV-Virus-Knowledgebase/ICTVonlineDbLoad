06991568a363d3cfe6ec0c6697c4c6d94bce8e8f
Curtis Hendrickson <curtish@uab.edu>
2022-03-01 15:08:23 -0600

    factor previous MSL out as a variable

--- 0.b1. rename old load_next_msl.sql
+++ 0.b1. rename old load_next_msl.sql
@@ -4,31 +4,33 @@
  */
 
 GO
+DECLARE @OLD_MSL varchar(32); SET @OLD_MSL='36'
 
-SELECT name, SCHEMA_NAME(schema_id) AS schema_name, type_desc, type  
+SELECT name, SCHEMA_NAME(schema_id) AS schema_name, type_desc, type, '>>>>>>', old_msl=@OLD_MSL  
 FROM sys.objects  
 WHERE parent_object_id = (OBJECT_ID('load_next_msl'))   
 AND type IN ('D','C','F', 'PK');   
-GO  
+ 
 
 --
 -- rename default constraints - that's a global namespace (ICK!)
 --
-exec sp_rename 'DF_load_next_msl__msl_release_num', 'DF_load_next_msl__msl_release_num_35';  
-GO
+DECLARE @NEW_DF varchar(200); SET @NEW_DF='DF_load_next_msl__msl_release_num_'+@OLD_MSL
+exec sp_rename 'DF_load_next_msl__msl_release_num', @NEW_DF;  
+
 
 -- 
 -- rename table
-EXEC sp_rename 'load_next_msl', 'load_next_msl_35';  
---EXEC sp_rename 'load_next_msl_34b', 'load_next_msl';  
+DECLARE @NEW_TAB varchar(200); SET @NEW_TAB='load_next_msl_'+@OLD_MSL
+EXEC sp_rename 'load_next_msl',@NEW_TAB ;  
 
-go
+
 
 --
 -- Verify
 --
 SELECT name, SCHEMA_NAME(schema_id) AS schema_name, type_desc, type  
 FROM sys.objects  
-WHERE parent_object_id = (OBJECT_ID('load_next_msl_35'))   
+WHERE parent_object_id = (OBJECT_ID('load_next_msl_'+@OLD_MSL))   
 AND type IN ('D','C','F', 'PK');   
 GO 
