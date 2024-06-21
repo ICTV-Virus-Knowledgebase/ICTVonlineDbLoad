5fe6ad2e2e0e2f31882933cee3ef171226c8d386
Curtis Hendrickson <curtish@uab.edu>
2024-06-21 17:36:15 -0500

    MSL39v3 data dump
     * EC 55, Jena, Germany, August 2023
     * Email ratification April 2024 (MSL #39); release v3, June 19, 2024
     * 6 realms, 10 kingdoms, 18 phyla, 2 subphyla, 41 classes, 81 orders, 11 suborders, 314 families, 200 subfamilies, 3522 genera, 84 subgenera, 14690 species

---
+++ data/export_current_msl_tables.bat
@@ -0,0 +1,32 @@
+@REM
+@REM export tables to tsv needed for proposal_validator's current_msl/ cache
+@REM
+@REM 20230614 MSL38
+@REM
+@REM basic method from 
+@REM   https://stackoverflow.com/questions/1355876/export-table-to-file-with-column-headers-column-names-using-the-bcp-utility-an
+@REM utf-8 (-f o:65001 ) from 
+@REM   https://stackoverflow.com/questions/41561658/i-need-dump-table-from-sql-server-to-csv-in-utf-8
+@REM
+
+
+@REM primary data tables
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[taxonomy_toc]"| findstr /v /c:"-" /b > "taxonomy_toc.utf8.txt"
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[taxonomy_node_export]"| findstr /v /c:"-" /b > "taxonomy_node_export.utf8.txt"
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[vmr]"| findstr /v /c:"-" /b > "vmr.utf8.txt"
+
+@REM CV tables
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[taxonomy_level]"| findstr /v /c:"-" /b > "taxonomy_level.utf8.txt"
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[taxonomy_molecule]"| findstr /v /c:"-" /b > "taxonomy_molecule.utf8.txt"
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[taxonomy_host_source]"| findstr /v /c:"-" /b > "taxonomy_host_source.utf8.txt"
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[taxonomy_change_in]"| findstr /v /c:"-" /b > "taxonomy_change_in.utf8.txt"
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[taxonomy_change_out]"| findstr /v /c:"-" /b > "taxonomy_change_out.utf8.txt"
+
+
+
+@REM cache tables
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[taxonomy_node_delta]"| findstr /v /c:"-" /b > "taxonomy_node_delta.utf8.txt"
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[taxonomy_node_merge_split]"| findstr /v /c:"-" /b > "taxonomy_node_merge_split.utf8.txt"
+
+@REM convenience views
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[virus_isolates]"| findstr /v /c:"-" /b > "virus_isolates.utf8.txt"
