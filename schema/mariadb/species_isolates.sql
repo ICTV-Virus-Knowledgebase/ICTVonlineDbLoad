56bb237ea217b52af00c9f399b07c76361a9ebaa
mimsLR <loganmims52@gmail.com>
2024-09-23 11:01:15 -0500

    Updated species isolates and taxonomy node tables: 1. Used better practices for the converted MariaDB tables scripts.

--- schema/mariadb/species_isolates.sql
+++ schema/mariadb/species_isolates.sql
@@ -1,32 +1,33 @@
-CREATE TABLE species_isolates (
-    isolate_id INT NOT NULL AUTO_INCREMENT,
-    taxnode_id INT NULL,
-    species_sort INT NULL,
-    isolate_sort INT NOT NULL DEFAULT 1,
-    species_name VARCHAR(100) NOT NULL,
-    isolate_type CHAR(1) NOT NULL,
-    isolate_names VARCHAR(500) NULL,
-    _isolate_name VARCHAR(500) AS (
-        CASE 
-            WHEN isolate_names LIKE '%;%' THEN LEFT(isolate_names, LOCATE(';', isolate_names) - 1)
-            ELSE isolate_names 
-        END
-    ) PERSISTENT,
-    isolate_abbrevs VARCHAR(255) NULL,
-    isolate_designation VARCHAR(500) NULL,
-    genbank_accessions VARCHAR(4000) NULL,
-    refseq_accessions VARCHAR(4000) NULL,
-    genome_coverage VARCHAR(50) NULL,
-    molecule VARCHAR(50) NULL,
-    host_source VARCHAR(50) NULL,
-    refseq_organism VARCHAR(255) NULL,
-    refseq_taxids VARCHAR(4000) NULL,
-    update_change VARCHAR(50) NULL,
-    update_prev_species VARCHAR(100) NULL,
-    update_prev_taxnode_id INT NULL,
-    update_change_proposal VARCHAR(512) NULL,
-    PRIMARY KEY (isolate_id)
-) ENGINE=InnoDB;
+CREATE TABLE `species_isolates` (
+  `isolate_id` INT AUTO_INCREMENT NOT NULL,
+  `taxnode_id` INT,
+  `species_sort` INT,
+  `isolate_sort` INT NOT NULL DEFAULT 1,
+  `species_name` VARCHAR(100) NOT NULL,
+  `isolate_type` CHAR(1) NOT NULL,
+  `isolate_names` VARCHAR(500),
+  `isolate_abbrevs` VARCHAR(255),
+  `isolate_designation` VARCHAR(500),
+  `genbank_accessions` VARCHAR(4000),
+  `refseq_accessions` VARCHAR(4000),
+  `genome_coverage` VARCHAR(50),
+  `molecule` VARCHAR(50),
+  `host_source` VARCHAR(50),
+  `refseq_organism` VARCHAR(255),
+  `refseq_taxids` VARCHAR(4000),
+  `update_change` VARCHAR(50),
+  `update_prev_species` VARCHAR(100),
+  `update_prev_taxnode_id` INT,
+  `update_change_proposal` VARCHAR(512),
+  PRIMARY KEY (`isolate_id`),
+  `isolate_name` VARCHAR(500) GENERATED ALWAYS AS (
+    CASE 
+      WHEN `isolate_names` LIKE '%;%' 
+      THEN LEFT(`isolate_names`, LOCATE(';', `isolate_names`) - 1) 
+      ELSE `isolate_names` 
+    END
+  ) PERSISTENT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Adding Foreign Key Constraints
 ALTER TABLE species_isolates 
--- schema/mariadb/taxonomy_node.sql
+++ schema/mariadb/taxonomy_node.sql
@@ -1,156 +1,161 @@
-CREATE TABLE taxonomy_node (
-    taxnode_id INT NOT NULL,
-    parent_id INT NULL,
-    tree_id INT NOT NULL,
-    msl_release_num INT NULL,
-    level_id INT NULL,
-    name VARCHAR(100) NULL,
-    ictv_id INT NULL,
-    molecule_id INT NULL,
-    abbrev_csv TEXT NULL,
-    genbank_accession_csv TEXT NULL,
-    genbank_refseq_accession_csv TEXT NULL,
-    refseq_accession_csv TEXT NULL,
-    isolate_csv TEXT NULL,
-    notes TEXT NULL,
-    is_ref INT NOT NULL DEFAULT 0,
-    is_official INT NOT NULL DEFAULT 0,
-    is_hidden INT NOT NULL DEFAULT 0,
-    is_deleted INT NOT NULL DEFAULT 0,
-    is_deleted_next_year INT NOT NULL DEFAULT 0,
-    is_typo INT NOT NULL DEFAULT 0,
-    is_renamed_next_year INT NOT NULL DEFAULT 0,
-    is_obsolete INT NOT NULL DEFAULT 0,
-    in_change VARCHAR(10) NULL,
-    in_target VARCHAR(255) NULL,
-    in_filename VARCHAR(255) NULL,
-    in_notes TEXT NULL,
-    out_change VARCHAR(10) NULL,
-    out_target VARCHAR(255) NULL,
-    out_filename VARCHAR(255) NULL,
-    out_notes TEXT NULL,
-    start_num_sort INT NULL DEFAULT NULL,
-    row_num VARCHAR(25) NULL,
-    filename VARCHAR(255) NULL,
-    xref VARCHAR(255) NULL,
-    realm_id INT NULL,
-    realm_kid_ct INT NULL,
-    realm_desc_ct INT NULL,
-    subrealm_id INT NULL,
-    subrealm_kid_ct INT NULL,
-    subrealm_desc_ct INT NULL,
-    kingdom_id INT NULL,
-    kingdom_kid_ct INT NULL,
-    kingdom_desc_ct INT NULL,
-    subkingdom_id INT NULL,
-    subkingdom_kid_ct INT NULL,
-    subkingdom_desc_ct INT NULL,
-    phylum_id INT NULL,
-    phylum_kid_ct INT NULL,
-    phylum_desc_ct INT NULL,
-    subphylum_id INT NULL,
-    subphylum_kid_ct INT NULL,
-    subphylum_desc_ct INT NULL,
-    class_id INT NULL,
-    class_kid_ct INT NULL,
-    class_desc_ct INT NULL,
-    subclass_id INT NULL,
-    subclass_kid_ct INT NULL,
-    subclass_desc_ct INT NULL,
-    order_id INT NULL,
-    order_kid_ct INT NULL,
-    order_desc_ct INT NULL,
-    suborder_id INT NULL,
-    suborder_kid_ct INT NULL,
-    suborder_desc_ct INT NULL,
-    family_id INT NULL,
-    family_kid_ct INT NULL,
-    family_desc_ct INT NULL,
-    subfamily_id INT NULL,
-    subfamily_kid_ct INT NULL,
-    subfamily_desc_ct INT NULL,
-    genus_id INT NULL,
-    genus_kid_ct INT NULL,
-    genus_desc_ct INT NULL,
-    subgenus_id INT NULL,
-    subgenus_kid_ct INT NULL,
-    subgenus_desc_ct INT NULL,
-    species_id INT NULL,
-    species_kid_ct INT NULL,
-    species_desc_ct INT NULL,
-    taxa_kid_cts VARCHAR(200) NULL,
-    taxa_desc_cts VARCHAR(200) NULL,
-    inher_molecule_id INT NULL,
-    left_idx INT NULL,
-    right_idx INT NULL,
-    node_depth INT NULL,
-    lineage VARCHAR(500) NULL,
-    cleaned_name VARCHAR(100) AS (
+CREATE TABLE `taxonomy_node` (
+  `taxnode_id` INT NOT NULL,
+  `parent_id` INT,
+  `tree_id` INT NOT NULL,
+  `msl_release_num` INT,
+  `level_id` INT,
+  `name` VARCHAR(100),
+  `ictv_id` INT,
+  `molecule_id` INT,
+  `abbrev_csv` LONGTEXT,
+  `genbank_accession_csv` LONGTEXT,
+  `genbank_refseq_accession_csv` LONGTEXT,
+  `refseq_accession_csv` LONGTEXT,
+  `isolate_csv` LONGTEXT,
+  `notes` LONGTEXT,
+  `is_ref` INT NOT NULL,
+  `is_official` INT NOT NULL,
+  `is_hidden` INT NOT NULL,
+  `is_deleted` INT NOT NULL,
+  `is_deleted_next_year` INT NOT NULL,
+  `is_typo` INT NOT NULL,
+  `is_renamed_next_year` INT NOT NULL,
+  `is_obsolete` INT NOT NULL,
+  `in_change` VARCHAR(10),
+  `in_target` VARCHAR(255),
+  `in_filename` VARCHAR(255),
+  `in_notes` LONGTEXT,
+  `out_change` VARCHAR(10),
+  `out_target` VARCHAR(255),
+  `out_filename` VARCHAR(255),
+  `out_notes` LONGTEXT,
+  `start_num_sort` INT,
+  `row_num` VARCHAR(25),
+  `filename` VARCHAR(255),
+  `xref` VARCHAR(255),
+  `realm_id` INT,
+  `realm_kid_ct` INT,
+  `realm_desc_ct` INT,
+  `subrealm_id` INT,
+  `subrealm_kid_ct` INT,
+  `subrealm_desc_ct` INT,
+  `kingdom_id` INT,
+  `kingdom_kid_ct` INT,
+  `kingdom_desc_ct` INT,
+  `subkingdom_id` INT,
+  `subkingdom_kid_ct` INT,
+  `subkingdom_desc_ct` INT,
+  `phylum_id` INT,
+  `phylum_kid_ct` INT,
+  `phylum_desc_ct` INT,
+  `subphylum_id` INT,
+  `subphylum_kid_ct` INT,
+  `subphylum_desc_ct` INT,
+  `class_id` INT,
+  `class_kid_ct` INT,
+  `class_desc_ct` INT,
+  `subclass_id` INT,
+  `subclass_kid_ct` INT,
+  `subclass_desc_ct` INT,
+  `order_id` INT,
+  `order_kid_ct` INT,
+  `order_desc_ct` INT,
+  `suborder_id` INT,
+  `suborder_kid_ct` INT,
+  `suborder_desc_ct` INT,
+  `family_id` INT,
+  `family_kid_ct` INT,
+  `family_desc_ct` INT,
+  `subfamily_id` INT,
+  `subfamily_kid_ct` INT,
+  `subfamily_desc_ct` INT,
+  `genus_id` INT,
+  `genus_kid_ct` INT,
+  `genus_desc_ct` INT,
+  `subgenus_id` INT,
+  `subgenus_kid_ct` INT,
+  `subgenus_desc_ct` INT,
+  `species_id` INT,
+  `species_kid_ct` INT,
+  `species_desc_ct` INT,
+  `taxa_kid_cts` VARCHAR(200),
+  `taxa_desc_cts` VARCHAR(200),
+  `inher_molecule_id` INT,
+  `left_idx` INT,
+  `right_idx` INT,
+  `node_depth` INT,
+  `lineage` VARCHAR(500),
+  `cleaned_name` VARCHAR(100) GENERATED ALWAYS AS (
     REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
     REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
     REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
-    REPLACE(REPLACE(name, 'í', 'i'), 'é', 'e'), 'ó', 'o'), 'ú', 'u'), 'á', 'a'),
+    REPLACE(REPLACE(`name`, 'í', 'i'), 'é', 'e'), 'ó', 'o'), 'ú', 'u'), 'á', 'a'),
     'ì', 'i'), 'è', 'e'), 'ò', 'o'), 'ù', 'u'), 'à', 'a'), 'î', 'i'), 'ê', 'e'),
     'ô', 'o'), 'û', 'u'), 'â', 'a'), 'ü', 'u'), 'ö', 'o'), 'ï', 'i'), 'ë', 'e'),
     'ä', 'a'), 'ç', 'c'), 'ñ', 'n'), '‘', ''''), '’', ''''), '`', ' '), '  ', ' '),
     'ā', 'a'), 'ī', 'i'), 'ĭ', 'i'), 'ǎ', 'a'), 'ē', 'e'), 'ō', 'o')
-) PERSISTENT,
-    cleaned_problem VARCHAR(100) AS (
+  ) PERSISTENT,
+  `cleaned_problem` VARCHAR(100) GENERATED ALWAYS AS (
     CASE 
-        WHEN INSTR(name, 'í') > 0 THEN 'í (accented i)'
-        WHEN INSTR(name, 'é') > 0 THEN 'é (accented e)'
-        WHEN INSTR(name, 'ó') > 0 THEN 'ó (accented o)'
-        WHEN INSTR(name, 'ú') > 0 THEN 'ú (accented u)'
-        WHEN INSTR(name, 'á') > 0 THEN 'á (accented a)'
-        WHEN INSTR(name, 'ì') > 0 THEN 'ì (accented i)'
-        WHEN INSTR(name, 'è') > 0 THEN 'è (accented e)'
-        WHEN INSTR(name, 'ò') > 0 THEN 'ò (accented o)'
-        WHEN INSTR(name, 'ù') > 0 THEN 'ù (accented u)'
-        WHEN INSTR(name, 'à') > 0 THEN 'à (accented a)'
-        WHEN INSTR(name, 'î') > 0 THEN 'î (accented i)'
-        WHEN INSTR(name, 'ê') > 0 THEN 'ê (accented e)'
-        WHEN INSTR(name, 'ô') > 0 THEN 'ô (accented o)'
-        WHEN INSTR(name, 'û') > 0 THEN 'û (accented u)'
-        WHEN INSTR(name, 'â') > 0 THEN 'â (accented a)'
-        WHEN INSTR(name, 'ü') > 0 THEN 'ü (accented u)'
-        WHEN INSTR(name, 'ö') > 0 THEN 'ö (accented o)'
-        WHEN INSTR(name, 'ï') > 0 THEN 'ï (accented i)'
-        WHEN INSTR(name, 'ë') > 0 THEN 'ë (accented e)'
-        WHEN INSTR(name, 'ä') > 0 THEN 'ä (accented a)'
-        WHEN INSTR(name, 'ç') > 0 THEN 'ç (accented c)'
-        WHEN INSTR(name, 'ñ') > 0 THEN 'ñ (accented n)'
-        WHEN INSTR(name, '‘') > 0 THEN '‘ (Microsoft curvy open single-quote)'
-        WHEN INSTR(name, '’') > 0 THEN '’ (Microsoft curvy close single-quote)'
-        WHEN INSTR(name, '`') > 0 THEN '` (ASCII back-quote)'
-        WHEN INSTR(name, '  ') > 0 THEN '(double space)'
-        WHEN INSTR(name, 'ā') > 0 THEN 'a-macron'
-        WHEN INSTR(name, 'ī') > 0 THEN 'i-macron'
-        WHEN INSTR(name, 'ĭ') > 0 THEN 'i-breve'
-        WHEN INSTR(name, 'ǎ') > 0 THEN 'a-caron'
-        WHEN INSTR(name, 'ē') > 0 THEN 'e-macron'
-        WHEN INSTR(name, 'ō') > 0 THEN 'o-macron'
+      WHEN INSTR(`name`, 'í') > 0 THEN 'í (accented i)'
+      WHEN INSTR(`name`, 'é') > 0 THEN 'é (accented e)'
+      WHEN INSTR(`name`, 'ó') > 0 THEN 'ó (accented o)'
+      WHEN INSTR(`name`, 'ú') > 0 THEN 'ú (accented u)'
+      WHEN INSTR(`name`, 'á') > 0 THEN 'á (accented a)'
+      WHEN INSTR(`name`, 'ì') > 0 THEN 'ì (accented i)'
+      WHEN INSTR(`name`, 'è') > 0 THEN 'è (accented e)'
+      WHEN INSTR(`name`, 'ò') > 0 THEN 'ò (accented o)'
+      WHEN INSTR(`name`, 'ù') > 0 THEN 'ù (accented u)'
+      WHEN INSTR(`name`, 'à') > 0 THEN 'à (accented a)'
+      WHEN INSTR(`name`, 'î') > 0 THEN 'î (accented i)'
+      WHEN INSTR(`name`, 'ê') > 0 THEN 'ê (accented e)'
+      WHEN INSTR(`name`, 'ô') > 0 THEN 'ô (accented o)'
+      WHEN INSTR(`name`, 'û') > 0 THEN 'û (accented u)'
+      WHEN INSTR(`name`, 'â') > 0 THEN 'â (accented a)'
+      WHEN INSTR(`name`, 'ü') > 0 THEN 'ü (accented u)'
+      WHEN INSTR(`name`, 'ö') > 0 THEN 'ö (accented o)'
+      WHEN INSTR(`name`, 'ï') > 0 THEN 'ï (accented i)'
+      WHEN INSTR(`name`, 'ë') > 0 THEN 'ë (accented e)'
+      WHEN INSTR(`name`, 'ä') > 0 THEN 'ä (accented a)'
+      WHEN INSTR(`name`, 'ç') > 0 THEN 'ç (accented c)'
+      WHEN INSTR(`name`, 'ñ') > 0 THEN 'ñ (accented n)'
+      WHEN INSTR(`name`, '‘') > 0 THEN '‘ (Microsoft curvy open single-quote)'
+      WHEN INSTR(`name`, '’') > 0 THEN '’ (Microsoft curvy close single-quote)'
+      WHEN INSTR(`name`, '`') > 0 THEN '` (ASCII back-quote)'
+      WHEN INSTR(`name`, '  ') > 0 THEN '(double space)'
+      WHEN INSTR(`name`, 'ā') > 0 THEN 'a-macron'
+      WHEN INSTR(`name`, 'ī') > 0 THEN 'i-macron'
+      WHEN INSTR(`name`, 'ĭ') > 0 THEN 'i-breve'
+      WHEN INSTR(`name`, 'ǎ') > 0 THEN 'a-caron'
+      WHEN INSTR(`name`, 'ē') > 0 THEN 'e-macron'
+      WHEN INSTR(`name`, 'ō') > 0 THEN 'o-macron'
     END
-) PERSISTENT,
-
-    flags VARCHAR(255) AS (
+  ) PERSISTENT,
+  `flags` VARCHAR(255) GENERATED ALWAYS AS (
     CONCAT(
-        IF(tree_id = taxnode_id, 'root;', ''),
-        IF(is_hidden = 1, 'hidden;', ''),
-        IF(is_deleted = 1, 'deleted;', ''),
-        IF(is_deleted_next_year = 1, 'removed_next_year;', ''),
-        IF(is_typo = 1, 'typo;', ''),
-        IF(is_renamed_next_year = 1, 'renamed_next_year;', ''),
-        IF(is_obsolete = 1, 'obsolete;', '')
+      CASE WHEN `tree_id` = `taxnode_id` THEN 'root;' ELSE '' END,
+      CASE WHEN `is_hidden` = 1 THEN 'hidden;' ELSE '' END,
+      CASE WHEN `is_deleted` = 1 THEN 'deleted;' ELSE '' END,
+      CASE WHEN `is_deleted_next_year` = 1 THEN 'removed_next_year;' ELSE '' END,
+      CASE WHEN `is_typo` = 1 THEN 'typo;' ELSE '' END,
+      CASE WHEN `is_renamed_next_year` = 1 THEN 'renamed_next_year;' ELSE '' END,
+      CASE WHEN `is_obsolete` = 1 THEN 'obsolete;' ELSE '' END
     )
-) VIRTUAL,
-    _numKids INT AS (((right_idx - left_idx) - 1) / 2) PERSISTENT,
-    _out_target_parent VARCHAR(1000) AS (TRIM(TRAILING FROM TRIM(LEADING FROM REVERSE(SUBSTRING(REPLACE(REVERSE(out_target), ';', REPEAT(' ', 1000)), 500, 1500))))) PERSISTENT,
-    _out_target_name VARCHAR(500) AS (TRIM(TRAILING FROM TRIM(LEADING FROM REVERSE(SUBSTRING(REPLACE(REVERSE(out_target), ';', REPEAT(' ', 1000)), 0, 500))))) PERSISTENT,
-    exemplar_name TEXT NULL,
-    genome_coverage VARCHAR(50) NULL,
-    host_source VARCHAR(50) NULL,
-    PRIMARY KEY (taxnode_id)
-) ENGINE=InnoDB;
+  ) PERSISTENT,
+  `_numKids` INT GENERATED ALWAYS AS (
+    ((`right_idx` - `left_idx`) - 1) / 2
+  ) PERSISTENT,
+  `_out_target_parent` VARCHAR(255) GENERATED ALWAYS AS (
+    RTRIM(LTRIM(REVERSE(SUBSTRING(REPLACE(REVERSE(`out_target`), ';', REPEAT(' ', 1000)), 500, 1500))))
+  ) PERSISTENT,
+  `_out_target_name` VARCHAR(255) GENERATED ALWAYS AS (
+    RTRIM(LTRIM(REVERSE(SUBSTRING(REPLACE(REVERSE(`out_target`), ';', REPEAT(' ', 1000)), 0, 500))))
+  ) PERSISTENT,
+  `exemplar_name` VARCHAR(255),
+  `genome_coverage` VARCHAR(50),
+  `host_source` VARCHAR(50),
+  PRIMARY KEY (`taxnode_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Foreign Key Constraints
 ALTER TABLE taxonomy_node
