5a07ef284d777e59b0931ead0d33e19201376bec
Curtis Hendrickson <curtish@uab.edu>
2025-01-13 13:43:39 -0600

    MSL39v4 +taxonomy_node_mariadb_etl, +virus_prop
    Need this taxonomy_node export to build MariaDB mirror, until update triggers/SPs are fully functional in MariaDB

--- data/export_current_msl_tables.bat
+++ data/export_current_msl_tables.bat
@@ -1,7 +1,8 @@
 @REM
 @REM export tables to tsv needed for proposal_validator's current_msl/ cache
 @REM
-@REM 20230614 MSL38
+@REM 20250113 CurtisH MSL39v4 +taxonomy_node_mariadb_etl, +virus_prop
+@REM 20230614 CurtisH MSL38
 @REM
 @REM basic method from 
 @REM   https://stackoverflow.com/questions/1355876/export-table-to-file-with-column-headers-column-names-using-the-bcp-utility-an
@@ -12,7 +13,13 @@
 
 @REM primary data tables
 sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[taxonomy_toc]"| findstr /v /c:"-" /b > "taxonomy_toc.utf8.txt"
+@REM used by ProposalQC
 sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[taxonomy_node_export]"| findstr /v /c:"-" /b > "taxonomy_node_export.utf8.txt"
+@REM used for ETL to MariaDB, until MariaDB is primary
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[taxonomy_node_mariadb_etl]"| findstr /v /c:"-" /b > "taxonomy_node_mariadb_etl.utf8.txt"
+
+sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[virus_prop]"| findstr /v /c:"-" /b > "virus_prop.utf8.txt"
+
 @REM replaced with species_isolates
 @REM sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[vmr]"| findstr /v /c:"-" /b > "vmr.utf8.txt"
 sqlcmd -s"	" -f o:65001 -W -Q "set nocount on; select * from [ICTVonline39].[dbo].[species_isolates]"| findstr /v /c:"-" /b > "species_isolates.utf8.txt"
